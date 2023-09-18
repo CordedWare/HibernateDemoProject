684afae3a58eab0ca4477856909391241fce8de4
Ivan Tretyakov <ivtretyakov56@gmail.com>
2023-09-18 21:01:30 +0500

    50. Criteria API

--- src/test/java/org/hiberproject/dao/UserDaoTest.java
+++ src/test/java/org/hiberproject/dao/UserDaoTest.java
@@ -3,6 +3,7 @@ package org.hiberproject.dao;
 import lombok.Cleanup;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hiberproject.dto.CompanyDto;
 import org.hiberproject.entity.Payment;
 import org.hiberproject.entity.User;
 import org.hiberproject.util.HibernateTestUtil;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import javax.persistence.Tuple;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -21,15 +23,15 @@ import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 @TestInstance(PER_CLASS)
 class UserDaoTest {
 
-    private final SessionFactory sessionFactory = HibernateTestUtil.buildSessionFactory();
-    private final UserDao userDao = UserDao.getInstance();
+    private final SessionFactory sessionFactory = HibernateTestUtil.buildSessionFactory(); // иницилизирую сессию 1 раз, чтобы каждый раз не поднимать для нового теста
+    private final UserDao userDao = UserDao.getInstance(); // от него идут все наши запросы
 
-    @BeforeAll
+    @BeforeAll // инициализирую сущности
     public void initDb() {
         TestDataImporter.importData(sessionFactory);
     }
 
-    @AfterAll
+    @AfterAll // после выполнения всех наших тестов я закрываю сессию
     public void finish() {
         sessionFactory.close();
     }
@@ -120,13 +122,13 @@ class UserDaoTest {
         @Cleanup Session session = sessionFactory.openSession();
         session.beginTransaction();
 
-        List<Object[]> results = userDao.findCompanyNamesWithAvgUserPaymentsOrderedByCompanyName(session);
+        List<CompanyDto> results = userDao.findCompanyNamesWithAvgUserPaymentsOrderedByCompanyName(session);
         assertThat(results).hasSize(3);
 
-        List<String> orgNames = results.stream().map(a -> (String) a[0]).collect(toList());
+        List<String> orgNames = results.stream().map(CompanyDto::getName).collect(toList());
         assertThat(orgNames).contains("Apple", "Google", "Microsoft");
 
-        List<Double> orgAvgPayments = results.stream().map(a -> (Double) a[1]).collect(toList());
+        List<Double> orgAvgPayments = results.stream().map(CompanyDto::getAmount).collect(toList());
         assertThat(orgAvgPayments).contains(410.0, 400.0, 300.0);
 
         session.getTransaction().commit();
@@ -137,13 +139,13 @@ class UserDaoTest {
         @Cleanup Session session = sessionFactory.openSession();
         session.beginTransaction();
 
-        List<Object[]> results = userDao.isItPossible(session);
+        List<Tuple> results = userDao.isItPossible(session);
         assertThat(results).hasSize(2);
 
-        List<String> names = results.stream().map(r -> ((User) r[0]).fullName()).collect(toList());
+        List<String> names = results.stream().map(r -> r.get(0, User.class).fullName()).collect(toList());
         assertThat(names).contains("Sergey Brin", "Steve Jobs");
 
-        List<Double> averagePayments = results.stream().map(r -> (Double) r[1]).collect(toList());
+        List<Double> averagePayments = results.stream().map(r -> r.get(1, Double.class)).collect(toList());
         assertThat(averagePayments).contains(500.0, 450.0);
 
         session.getTransaction().commit();
